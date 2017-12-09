b59c69e055c6dcce7825bb3371cd284336e583bf
Jared Pleva <jpleva91@gmail.com>
2017-12-09 11:24:24 -0700

    adds constructor and updates state in componentWillMount()

--- client/main.js
+++ client/main.js
@@ -8,13 +8,20 @@ import ImageList from './components/image_list';
 
 // Create a component
 class App extends Component {
-  componentWillMount() {
-    // Fantastic place to load data!
-    axios.get(`https://api.imgur.com/3/gallery/hot/viral/0`)
-      .then(response => console.log(response));
+  constructor(props) {
+    super(props);
+
+    this.state = { images: [] };
   }
 
-  render() {  
+  componentWillMount() {
+    // Fantastic place to load data!
+    axios.get('https://api.imgur.com/3/gallery/hot/viral/0')
+      .then(response => this.setState({ images: response.data.data }));
+  }
+
+  render() {
+    console.log(this.state.images)
     return (
       <div>
         <ImageList />
