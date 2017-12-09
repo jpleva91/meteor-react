385e8b02c9debb08d17f904b9b64d733e6f765be
Jared Pleva <jpleva91@gmail.com>
2017-12-08 23:35:18 -0700

    adds ReactDOM library & Meteor.startup() to main.js

--- client/main.js
+++ client/main.js
@@ -2,8 +2,18 @@
 
 // Import the React library
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 // Create a component
-
+const App = () => {
+  return (
+    <div>
+      React App #2
+    </div>
+  );
+};
 
 // Render this component to the screen
+Meteor.startup(() => {
+  ReactDOM.render(<App />, document.querySelector('.container'));
+})
