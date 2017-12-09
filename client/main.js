7dc39cff47531717430724b44549a106394cb64b
Jared Pleva <jpleva91@gmail.com>
2017-12-09 11:14:43 -0700

    implements axios in main.js

--- client/main.js
+++ client/main.js
@@ -1,20 +1,29 @@
 // Any JS in here is automatically ran
 
 // Import the React library
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import ImageList from './components/image_list';
 
 // Create a component
-const App = () => {
-  return (
-    <div>
-      <ImageList />
-    </div>
-  );
+class App extends Component {
+  componentWillMount() {
+    // Fantastic place to load data!
+    axios.get(`https://api.imgur.com/3/gallery/hot/viral/0`)
+      .then(response => console.log(response));
+  }
+
+  render() {  
+    return (
+      <div>
+        <ImageList />
+      </div>
+    );
+  }
 };
 
 // Render this component to the screen
 Meteor.startup(() => {
   ReactDOM.render(<App />, document.querySelector('.container'));
-})
+});
