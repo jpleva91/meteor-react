70260960dea68cb78e3de8e4782440b18ac7e6f0
Jared Pleva <jpleva91@gmail.com>
2017-12-08 21:27:04 -0700

    import React to main.js

--- client/main.js
+++ client/main.js
@@ -1,1 +1,9 @@
-console.log("hello!");
+// Any JS in here is automatically ran
+
+// Import the React library
+import React from 'react';
+
+// Create a component
+
+
+// Render this component to the screen
