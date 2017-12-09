81685df6fd84a8feb10092e8e9fa281d21d04d8b
Jared Pleva <jpleva91@gmail.com>
2017-12-08 23:58:15 -0700

    adds image_detail component to image_list.js

--- client/components/image_list.js
+++ client/components/image_list.js
@@ -1,12 +1,19 @@
 // Create image list component
 // Import React
 import React from 'react';
+import ImageDetail from './image_detail';
+
+const IMAGES = [
+  { title:'Pen', link: 'https://dummyimage.com/600x400'},
+  { title: 'Pine Tree', link: 'https://dummyimage.com/600x400'},
+  { title: 'Mug', link: 'https://dummyimage.com/600x400'}
+];
 
 // Create component
 const ImageList = () => {
   return (
     <ul>
-      <li>Image Detail</li>
+      <ImageDetail />
     </ul>
   );
 };
