e3c7eff42e82f1ed831519d95a05853f2500e89d
Jared Pleva <jpleva91@gmail.com>
2017-12-08 23:45:43 -0700

    Creates image_list component

---
+++ client/components/image_list.js
@@ -0,0 +1,15 @@
+// Create image list component
+// Import React
+import React from 'react';
+
+// Create component
+const ImageList = () => {
+  return (
+    <ul>
+      <li>Image Detail</li>
+    </ul>
+  );
+};
+
+// Export component
+export default ImageList;
